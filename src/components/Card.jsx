f0768a16b2343983b8af6e477168b1682b0c9221
Jh0nC <jhonatancorrales681@gmail.com>
2024-04-23 01:08:17 -0500

    se deja listo y funcional la implementacion de las targetas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,16 @@
 function Card(data) {
-  
+  return (
+
+    <div className="card">
+      <div className="card-head">
+        <h1>{data.data['name']}</h1>
+      </div>
+      <div className="card-body">
+        <p>{data.data['description']}</p>
+      </div>
+    </div>
+
+  )
 }
 
 export default Card;
--- src/pages/admin/modules/production/Products.jsx
+++ src/pages/admin/modules/production/Products.jsx
@@ -1,10 +1,22 @@
 import Card from "../../../../components/Card";
+import data from "../../../../../public/json/cardFile.json"
+
+var cardsData = data;
+console.log(cardsData);
 
 function Products() {
   return (
     <>
       <div className="container-fluid border-type-mid rounded-4 content py-3 px-2 bg-light shadow">
-        
+        <div className="row row-cols-4">
+          {
+            cardsData.map(item => (
+              <div className="col py-2 px-3" key={item.id}>
+                <Card data={item} />
+              </div>
+            ))
+          }
+        </div>
       </div>
     </>
   )
