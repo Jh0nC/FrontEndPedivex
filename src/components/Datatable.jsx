b537a28a4b78ed253c0ea90e917c920ab4563839
Jh0nC <Jh0nC>
2024-08-19 17:01:54 -0500

    embeces

--- src/components/Datatable.jsx
+++ src/components/Datatable.jsx
@@ -1,53 +1,65 @@
-import { Link } from "react-router-dom";
+import '../../public/css/datatableStyles.css';
 
-function Datatable(info) {
+function Datatables({ data }) {
   return (
-    <>
-      <div className="datatable-container border mx-auto">
-        <div className="datatable_header">
-          <h2>{info.title}</h2>
-          <button>Agregar {info.title}</button>
+    <div className="datatable-container border rounded-4 mx-auto my-3">
+      <div className="datatable_header">
+        <h2>{data.title}</h2>
+        <button>Agregar {data.module}</button>
 
-          <div className="input_search">
-            <input type="search" placeholder="Buscar" />
-            <i className="bi bi-search" id="search"></i>
-          </div>
-        </div>
-        <table className="datatable">
-          <thead>
-            {
-              info.colNames.map(item => {
-                <tr>
-                  <th>{item.colName}<i className="bi bi-chevron-expand"></i></th>
-                </tr>
-              })
-            }
-          </thead>
-          <tbody>
-            {
-              info.content.map(data => {
-                <tr>
-                  <td>(field)</td>
-                  <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                      <i className="bi bi-pencil-square" id="icons"></i>
-                    </button>
-                  </td>
-                </tr>
-              })
-            }
-          </tbody>
-        </table>
-        <div className="datatable_fotter">
-          <p>(some info)</p>
+        <div className="input_search">
+          <input type="search" placeholder="Buscar" />
+          <i className="bi bi-search" id="search"></i>
         </div>
       </div>
-    </>
-  )
+      <table className="datatable">
+        <thead>
+          <tr>
+            {data.colNames && data.colNames.map((col, index) => (
+              <th key={index}>
+                {col} <i className="bi bi-chevron-expand"></i>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.content && data.content.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.stock + ' ' + item.unit}</td>
+              <td>{item.state}</td>
+              <td>
+                <button>See</button>
+                <button>Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="datatable_fotter d-flex justify-content-between align-items-center">
+        <p>Total de filas : 05</p>
+
+
+        <button className="btn btn-outline-success rounded-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-filetype-pdf"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"
+            />
+          </svg>
+          Generar Excel
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Datatable;
-
-
-
-
+export default Datatables;
--- src/pages/admin/modules/production/Supplies.jsx
+++ src/pages/admin/modules/production/Supplies.jsx
@@ -1,36 +1,130 @@
-import { useState, useEffect } from "react";
 import Datatable from "../../../../components/Datatable";
 
 function Supplies() {
-  
-  const [content, setContent] = useState(null);
-  const url = "http://localhost:2145/supplie";
 
-  useEffect(() => {
-
-    async function getData(url) {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setContent(data); // Aquí guardamos los datos en el estado
-      } catch (err) {
-        console.log(err);
+  const data = {
+    module: "insumo",
+    title: "Insumos",
+    colNames: ["Id", "Nombre", "Stock", "Estado", "Acciones"],
+    content: [
+      {
+        "id": 1,
+        "name": "harina",
+        "stock": 49895.2,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 2,
+        "name": "azúcar blanca",
+        "stock": 24947.6,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 3,
+        "name": "sal fina",
+        "stock": 9979.03,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 4,
+        "name": "levadura seca instantánea",
+        "stock": 4989.52,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 5,
+        "name": "mantequilla sin sal",
+        "stock": 10,
+        "unit": "unit",
+        "state": 1
+      },
+      {
+        "id": 6,
+        "name": "huevos",
+        "stock": 360,
+        "unit": "unit",
+        "state": 1
+      },
+      {
+        "id": 7,
+        "name": "leche entera",
+        "stock": 20000,
+        "unit": "ml",
+        "state": 1
+      },
+      {
+        "id": 8,
+        "name": "chocolate semi-amargo",
+        "stock": 4989.52,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 9,
+        "name": "aceite vegetal",
+        "stock": 5000,
+        "unit": "ml",
+        "state": 1
+      },
+      {
+        "id": 10,
+        "name": "hojaldres",
+        "stock": 9071.85,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 11,
+        "name": "salchicha ranchera",
+        "stock": 30,
+        "unit": "unit",
+        "state": 1
+      },
+      {
+        "id": 12,
+        "name": "arequipe",
+        "stock": 4535.92,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 13,
+        "name": "bocadillo",
+        "stock": 5443.11,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 14,
+        "name": "queso mozarrela",
+        "stock": 29937.1,
+        "unit": "gr",
+        "state": 1
+      },
+      {
+        "id": 15,
+        "name": "cuajada",
+        "stock": 7711.07,
+        "unit": "gr",
+        "state": 1
       }
-    }
-
-    getData(url);
-  }, [url]);
-
-  if (!content) {
-    return <div>Loading...</div>; // Muestra un mensaje mientras los datos se cargan
+    ]
   }
+
+  console.log(data);
+
 
   return (
     <>
-      <h1>{JSON.stringify(content)}</h1>
-      {/* Aquí puedes renderizar los datos de la manera que necesites */}
+      <div className="container-fluid border-type-mid rounded-4 content py-3 px-2 bg-light shadow">
+        <Datatable data={data} />
+      </div>
     </>
-  );
+  )
 }
 
 export default Supplies;
