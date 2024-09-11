4990c166b973bb50be0a03806d548a0b42d8a325
McDamian12 <damimono4@gmmail.com>
2024-09-10 23:57:47 -0500

    DM: Editar clientes y empleados

--- src/components/DatatableClients.jsx
+++ src/components/DatatableClients.jsx
@@ -1,59 +1,22 @@
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import '../../public/css/datatableStyles.css';
 
-import { Link } from 'react-router-dom';
+function Datatables({ data }) {
+  const navigate = useNavigate();
+  const [selectedClientId, setSelectedClientId] = useState(null);
 
-function DatatableClients({ data = [] }) {
   return (
     <div className="datatable-container border rounded-4 mx-auto my-3">
       <div className="datatable_header">
-        <h2>Clientes</h2>
+        <h2>{data.title}</h2>
+
         <div className="input_search">
           <input type="search" placeholder="Buscar" />
           <i className="bi bi-search" id="search"></i>
         </div>
-      </div>
-      <table className="datatable">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Correo Electrónico</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Documento</th>
-            <th>Dirección</th>
-            <th>Teléfono</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.length > 0 ? (
-            data.map((item, index) => (
-              <tr key={index}>
-                <td>{item.id}</td>
-                <td>{item.mail}</td>
-                <td>{item.firstName}</td>
-                <td>{item.lastName}</td>
-                <td>{item.document}</td>
-                <td>{item.address}</td>
-                <td>{item.phoneNumber}</td>
-                <td className="d-flex gap-2">
-                  <Link className="btn btn-warning rounded-5" to={`edit/${item.id}`}>
-                    Editar
-                    <i class="bi bi-pencil-square"></i>
-                  </Link>
-                  
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              
-            </tr>
-          )}
-        </tbody>
-      </table>
-      <div className="datatable_fotter d-flex justify-content-between align-items-center">
-        <p>Total de filas: {data.length}</p>
-        <button className="btn btn-outline-success rounded-5">
+
+        <button className="btn btn-success rounded-5">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -67,11 +30,43 @@ function DatatableClients({ data = [] }) {
               d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"
             />
           </svg>
-          Generar Excel
         </button>
+      </div>
+      <table className="datatable">
+        <thead>
+          <tr>
+            {data.colNames && data.colNames.map((col, index) => (
+              <th key={index}>
+                {col} <i className="bi bi-chevron-expand"></i>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.content && data.content.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.firstName + ' ' + item.lastName}</td>
+              <td>{item.document}</td>
+              <td>{item.address}</td>
+              <td>{item.phoneNumber}</td>
+              <td>
+                <Link
+                  className='btn btn-warning rounded-5'
+                  to={`/admin/users/edit/${item.id}`}
+                >
+                  Editar
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="datatable_fotter d-flex justify-content-between align-items-center">
+        <p>Total de filas: {data.content.length || 0}</p>
       </div>
     </div>
   );
 }
 
-export default DatatableClients;
+export default Datatables;
--- src/main.jsx
+++ src/main.jsx
@@ -51,11 +51,10 @@ import CreateCategory from "./pages/admin/modules/production/porductCategories/C
 import Sales from "./pages/admin/modules/sales/Sales";
 
 import Clients from "./pages/admin/modules/clients/Clients";
-// import ClientCreate from "./pages/admin/modules/clients/ClientCreate";
-// import ClientEdit from "./pages/admin/modules/clients/ClientEdit";
+import ClientsEdit from "./pages/admin/modules/clients/ClientEdit";
 
 import Employees from "./pages/admin/modules/employees/Employees";
-// import EmployeeCreate from "./pages/admin/modules/employees/EmployeeCreate";
+
 // import EmployeeEdit from "./pages/admin/modules/employees/EmployeeEdit";
 
 
@@ -114,7 +113,7 @@ ReactDOM.createRoot(document.querySelector('#root')).render(
             <Route path='roleEdit/:id' element={<RoleEdit />} />
             <Route path='users' element={<Users />} />
             <Route path='userCreate' element={<UsersCreate />} />
-            <Route path='userEdit/:id' element={<UsersEdit />} />
+            <Route path='users/edit/:id' element={<UsersEdit />} />
 
             <Route path='supplies-create' element={<SuppliesCreate />} />
             <Route path='products/create' element={<CreateProducts />} />
@@ -136,6 +135,7 @@ ReactDOM.createRoot(document.querySelector('#root')).render(
 
             {/* Dm */}
             <Route path='clients' element={<Clients />} />
+            {/* <Route path='clientsEdit/:id' element={<ClientsEdit />} /> */}
             {/* <Route path='clientCreate' element={<ClientCreate />} /> */}
             {/* <Route path='clientEdit' element={<ClientEdit />} /> */}
 
