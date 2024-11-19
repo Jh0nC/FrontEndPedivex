6e4ca79df4c970645ca05ba465b6144c39a2c8a6
Jh0nC <jhonatancorrales681@gmail.com>
2024-11-19 01:00:49 -0500

    correccionn del login

--- src/pages/client/Catalogue.jsx
+++ src/pages/client/Catalogue.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import productImg from "../client/Panzeroti.png"; // Imagen local
+import productImg from "../../../public/assets/products/panzerotti_arequipe.jpg"; // Imagen local
 import Footer from "../../partials/Footer";
 
 function Catalogue() {
@@ -8,7 +8,7 @@ function Catalogue() {
   // Función para consumir la API y obtener los productos
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch("https://pedivexapi.onrender.com/product");
+      const response = await fetch("http://localhost:3000/product");
       const data = await response.json();
       setProducts(data);
     };
@@ -27,27 +27,36 @@ function Catalogue() {
           </div>
         </div>
         <hr />
-        <div className="row justify-content-center g-4">
-          {products.filter(product => product.inCatalogue).map(product => (
-            <div key={product.id} className="col-12 col-sm-6 col-md-4 col-lg-4">
-              <div className="card shadow-sm border-0 rounded-3">
-                <img src={productImg} className="card-img-top rounded-3" alt={product.name} />
-                <div className="card-body text-center">
-                  <h5 className="card-title fs-5">{product.name}</h5>
-                  <p className="card-text text-muted">Precio: ${product.price}</p>
-                  <p className="card-text">
-                    Ingredientes principales:
-                    <ul className="list-unstyled">
-                      {product.datasheet.datasheetDetails.slice(0, 3).map(ingredient => (
-                        <li key={ingredient.id} className="small text-info">{ingredient.supply.name}</li>
-                      ))}
-                    </ul>
-                  </p>
-                  <a href="#" className="btn btn-primary btn-sm">Agregar al carrito</a>
-                </div>
+        <div class="container-fluid">
+          <div className="row">
+            <div className="col-4">
+              
+            </div>
+            <div className="col-8">
+              <div className="row">
+                {products.filter(product => product.inCatalogue).map(product => (
+                  <div key={product.id} className="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div className="card shadow-sm border-0 rounded-3">
+                      <img src={productImg} className="card-img-top rounded-3" alt={product.name} />
+                      <div className="card-body text-center">
+                        <h5 className="card-title fs-5">{product.name}</h5>
+                        <p className="card-text text-muted">Precio: ${product.price}</p>
+                        <p className="card-text">
+                          Ingredientes principales:
+                          <ul className="list-unstyled">
+                            {product.datasheet.datasheetDetails.slice(0, 3).map(ingredient => (
+                              <li key={ingredient.id} className="small text-info">{ingredient.supply.name}</li>
+                            ))}
+                          </ul>
+                        </p>
+                        <a href="#" className="btn btn-primary btn-sm">Agregar al carrito</a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
       <Footer />
